dfb1e65adf3641d6a9ddfcf0dec4ef4f8ff97795
reimber <reimber@gmail.com>
2023-12-21 16:08:58 +0000

    Updated with both sections of the Throat and Exit combined both and plotted

--- Nozzle.py
+++ Nozzle.py
@@ -2,7 +2,7 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
-import math
+
 # Set Constant Variables
 Engine_PO = 15
 Engine_TO = 2500
@@ -28,9 +28,7 @@ Exit_SpeedAtExit = Throat_M_Ex_Engine * np.sqrt((Engine_Y * Engine_Rspec * Exit_
 Throat_M_dot = Engine_Thrust / Exit_SpeedAtExit
 Throat_AreaOfThroat = Throat_M_dot / (Throat_sos * Throat_rho)
 Throat_R = np.sqrt(Throat_AreaOfThroat / np.pi)
-Exit_Ae = Throat_AreaOfThroat * np.sqrt(
-    (1 / Exit_Me ** 2) * (2 / (Engine_Y + 1) * (1 + ((Engine_Y - 1) / 2) * Exit_Me ** 2)) ** (
-                (Engine_Y + 1) / (Engine_Y - 1)))
+Exit_Ae = Throat_AreaOfThroat * np.sqrt((1 / Exit_Me ** 2) * (2 / (Engine_Y + 1) * (1 + ((Engine_Y - 1) / 2) * Exit_Me ** 2)) ** ((Engine_Y + 1) / (Engine_Y - 1)))
 Exit_RE = np.sqrt(Exit_Ae / np.pi)
 ExpansionRatio = Exit_Ae / Throat_AreaOfThroat
 sqrtEpsilon = np.sqrt(Exit_RE / Throat_R)
@@ -40,9 +38,18 @@ alpha = 15 * np.pi / 180
 Xn = R1 * np.sin(alpha)
 Yn = (Throat_R + R1) - (R1 * np.cos(alpha))
 Ln = (Exit_RE - Throat_R + R1 * (np.cos(alpha) - 1)) / (np.tan(alpha))
-L = ExpansionRatio + R1 * np.sin(alpha)
+L = Ln + R1 * np.sin(alpha)
 L1 = R1 * np.sin(alpha)
+XN = 0.0961429549062482
+XE = 1.44886536002306
 
+# General Varibles
+XY_ListComp = []
+NoOfPoints = 10
+XnList = []
+YnList = []
+Xn2List = []
+Yn2List = []
 def CalcVals():
     print('\nThese are all calculations based on initial values of PO,To,Mol Mass,Cp,Pe,R_Uni and Throat_R :-\n')
     print('Engine_PO_Pa', Engine_PO_Pa)
@@ -73,45 +80,72 @@ def CalcVals():
     print('Ln', Ln)
     print('L', L)
     print('L1', L1)
-    print(XnArray)
-    print(YnArray)
 
 
 
+# Create an array List holding all the calculated points Based on the end Value of Xn and zero
+# For Inlet and Throat
+# Add Zero to first position
+XnList.append(0)
+for i in range(1,NoOfPoints, 1):
+    XnList.append(Xn/NoOfPoints * i)
 
+# Append with end calculated Value Xn
+XnList.append(Xn)
+#XnArray = np.array([XnList])
 
+# Create an array List holding all the calculated points Based on the end Value of Yn Taking into account the Zero value of Xn
+# For Inlet and Throat
+for j in range(0,NoOfPoints,1):
 
-NoOfPoints = 80
-XnList = []
-YnList = []
+    YnList.append(R1 * (1 - np.cos(np.arcsin(XnList[j] / R1))) + Throat_R)
+    # else:
+    #     YnList.append(R1 * (1 - np.cos(np.arcsin(0 / R1))) + Throat_R)
+# Append with end calculated Value Yn
+YnList.append(Yn)
 
-# Create an array holding all the calculated points Based on the end Value of Xn and zero
-for i in range(0, NoOfPoints -1, 1):
-    XnList.append(Xn / NoOfPoints * i)
-XnArray = np.array([XnList])
+# Create Second Part of Graph (Linear Section) for X (ignoring 0)
 
-# Create an array holding all the calculated points Based on the end Value of Yn Taking into account the Zero value of Xn
-for j in range(0,NoOfPoints-1,1):
-    if j >= 1:
-        YnList.append(R1*(1-np.cos(np.arcsin(XnList[j-1]/R1)))+Throat_R)
+for k in range(0,NoOfPoints,1):
+
+    if k == 0:
+        Xn2List.append(Xn)
+    elif k == 1:
+        Xn2List.append(XN + (XE / (len(XnList)-2) * k))
     else:
-        YnList.append(R1 * (1 - np.cos(np.arcsin(0 / R1))) + Throat_R)
-YnArray = np.array([YnList])
+        Xn2List.append(XN + ((XE-XN) / (len(XnList) - 2) * k))
 
+
+for l in range(0,NoOfPoints,1):
+    # Place Last item from previous non linear calc into firt item in new list
+
+    if l == 0:
+        Yn2List.append(YnList[len(YnList)-1])
+    # Use previous item in new list for next list item calc
+    if l >= 1:
+        Yn2List.append(Yn2List[l-1]+(Exit_RE-Throat_R)/9)
+
+# Add final item to list as it is a pre-calculated Item
+Yn2List.append(Exit_RE)
+
+XnList.extend(Xn2List)
+YnList.extend(Yn2List)
+print(XnList)
+print(YnList)
 # Create DataFrame and load XnArray and YnArray in as X and Y values
 snsList = []
 
-for i in range(0,len(XnList)):
+for i in range(0,len(XnList)-1):
     snsList.append([XnList[i],YnList[i]])
+
 
 dataframe = pd.DataFrame(snsList, columns=['X', 'Y'])
 print(dataframe)
-
 sns.lineplot(data=dataframe,x='X',y='Y')
-sns.pointplot()
+plt.title("Linear Nozzle")
 plt.show()
 
 
 
 
-# CalcVals()
+#CalcVals()
